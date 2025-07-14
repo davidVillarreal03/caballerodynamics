6231ba08b3de0ad7e3307be20564ea0c353f0f4f
unknown <davidvillarr3al@gmail.com>
2025-07-13 22:14:54 -0500

    Added the nav bar style

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,13 +1,21 @@
 import { Link, useLocation } from 'react-router-dom';
+import { IMAGE_URL } from '../../constants';
 
 function NavTabs() {
     const currentPage = useLocation().pathname;
 
     return (
-        <div className='fixed top-0 w-full z-50 bg-white shadow-md'>
-            <nav className = "">
-                <ul className = "nav nav-tabs">    
-                    <li className = "nav-item">
+        <div className='flex fixed top-0 left-0 w-full bg-[#A89F94] shadow-md'>
+            <section className='flex items-center justify-between w-full px-4 py-2'>
+            <img className='h-16' src={IMAGE_URL} alt="logo"></img>
+            <button className='flex items-center'>
+            <i class="fa-solid fa-bars text-2xl text-[#D9D9D9] p-4"></i>
+            </button>
+
+            </section>
+            {/* <nav className = "">
+                <ul className = "flex gap-4 p-4">    
+                    <li className = "b">
                         <Link
                             to="/"
                             className={currentPage === "/" ? "nav-link active" : "nav-link"}
@@ -20,7 +28,7 @@ function NavTabs() {
                             to="/Portfolio"
                             className={currentPage === "/Portfolio" ? "nav-link active" : "nav-link"}
                         >
-                        Portfolio
+                        Courses
                         </Link>
                     </li>   
                     <li className = "nav-item">
@@ -36,11 +44,11 @@ function NavTabs() {
                             to="/Resume"
                             className={currentPage === "/Resume" ? "nav-link active" : "nav-link"}
                         >
-                        Resume
+                        Resources
                         </Link>
                     </li>   
                 </ul>
-            </nav>
+            </nav> */}
         </div>
     );
 }
